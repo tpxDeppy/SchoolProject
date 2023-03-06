85db5478ca69c949ef902336f3dd5efb2c9c4887
tpxDeppy <despoina.tsounaka@tpximpact.com>
2023-03-06 19:46:59 +0000

    update School.cs

--- SchoolProject.BL/Models/School.cs
+++ SchoolProject.BL/Models/School.cs
@@ -2,10 +2,11 @@
 
 namespace SchoolProject.Models
 {
-    public class School
+    internal class School
     {
         public Guid School_ID { get; set; } //PK
         public string School_name { get; set; }
 
+        public ICollection<Person> Persons { get; set; }
     }
 }
