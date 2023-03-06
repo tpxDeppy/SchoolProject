8b00c297fa5ef5da694c599c6ff5fe1f89cc0a08
tpxDeppy <despoina.tsounaka@tpximpact.com>
2023-03-06 12:43:00 +0000

    Project setup and .gitignore file

---
+++ SchoolProject.BL/Models/School.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolProject.Models
+{
+    public class School
+    {
+        public int User_ID { get; set; }
+        public string School_name { get; set; } = String.Empty;
+    }
+}
