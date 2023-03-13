f3dbfc34fc61c970f5a39030bbbb0eb0bd4d32b3
tpxDeppy <despoina.tsounaka@tpximpact.com>
2023-03-13 12:48:43 +0000

    Create UpdatePerson, Fix GetPupilsByYearGroup, Add Try Catch blocks

--- SchoolProject.BL/Models/Person.cs
+++ SchoolProject.BL/Models/Person.cs
@@ -5,8 +5,8 @@ namespace SchoolProject.BL.Models
     public class Person
     {
         public Guid User_ID { get; set; }
-        public string First_name { get; set; }
-        public string Last_name { get; set; }
+        public string First_name { get; set; } = string.Empty;
+        public string Last_name { get; set; } = string.Empty;
         public UserType User_type { get; set; }
         public DateTime? Date_of_birth { get; set; } //can be null
         public int? Year_group { get; set; } //can be null
